92ca232ae8ef05334fefb75d18b4a25461756f32
Ahmad Dzulfikar Ubaidillah <dzulfikarubaid@gmail.com>
2023-08-31 20:44:11 +0700

    update

--- src/lib/firebase/init2.ts
+++ src/lib/firebase/init2.ts
@@ -8,9 +8,11 @@ const firebaseConfig2 = {
   apiKey: process.env.FIREBASE_API_KEY2,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN2,
   projectId: process.env.FIREBASE_PROJECT_ID2,
+  databaseURL: process.env.FIREBASE_DATABASE_URL2,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET2,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID2,
-  appId: process.env.FIREBASE_APP_ID2
+  appId: process.env.FIREBASE_APP_ID2,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID2
 };
 
 // Initialize Firebase
