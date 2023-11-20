525caf21e9a97b6fe97ec4020ec471a2a5f52ab5
dzulfikarubaid <macbook@MacBooks-MacBook-Pro.local>
2023-11-20 17:18:15 +0700

    update design

---
+++ src/pages/new/navigation/index.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='flex fixed top-0 py-6 items-center px-20 flex-row w-full justify-between backdrop-blur-3xl bg-transparent text-teks '>
+    <div className='flex flex-row gap-3 justify-center items-center'>
+    <img className='w-[35px]  rounded-xl p-1 border-[0.5px] border-white/20' src="/logo-aeli-putih.png" alt="" />
+    <h1>AELI</h1>
+    </div>
+    <div className='flex flex-row gap-6 border-[1px] border-white/20 rounded-2xl p-2 px-4 font-extralight text-sm'>
+    <Link href={'/about'}>About</Link>
+    <Link href={'/activities'}>Activities</Link>
+    <Link href={'/members'}>Members</Link>
+    <Link href={'/blog'}>Blog</Link>
+    </div>
+    <div className='flex flex-row gap-4 items-center'>
+    <Link href={'/signin'}>Sign In</Link>
+    <Link className='border-[1px] border-white/20 rounded-2xl p-2 px-4'  href={'/signup'}>Sign Up</Link>
+    </div>
+    </div>
+  )
+}
+function WithNavbar(props:any){
+
+return(
+    <div className='bg-dongker h-fit w-full'>
+    <Navigation></Navigation>
+    <div className='h-[85px] flex justify-center items-center'>
+     <h1 className='text-[60px] font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-blue-900 via-purple-500 to-pink-300'>Build Capacity With AELI</h1>
+    </div>
+    </div>
+)
+}
+
+export default WithNavbar
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,20 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+  
     extend: {
+              colors:{
+        'dongker': '#0d0624',
+        'teks':'#e7e7ea'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+        scale: {
+            '-100': '-1',
+        },
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
       }
